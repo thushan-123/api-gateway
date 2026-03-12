8edf0a0b7c0d3b6b19ec143ed95c95c76eeab6dd
thushan-123 <thushanmadusanka456@gmail.com>
2026-03-12 06:50:11 +0530

    crete initial logs

--- logs.go
+++ logs.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func InitLogs() {
+	logFile, err := os.OpenFile("api_gateway.log", os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("fail to open api_gateway log file", err)
+		return
+	}
+
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(multiWriter)
+}
